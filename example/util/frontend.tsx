b8f6cd9faf6b350d19ff3a62701e989c032768ef
rbnis <5955614+rbnis@users.noreply.github.com>
2021-11-19 21:29:39 +0100

    fix: Better UI alignment

--- example/util/frontend.tsx
+++ example/util/frontend.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export function LabeledSwitch<T>({label, ...rest}: {label: string} & SwitchProps) {
   return (
-    <Form.Item label={label} labelCol={{ span: 4 }}  wrapperCol={{ span: 14 }} style={{textAlign: 'end'}}>
+    <Form.Item label={label} labelCol={{ span: 20 }} wrapperCol={{ span: 4 }} labelAlign='left'>
       <Switch {...rest}/>
     </Form.Item>
   )
