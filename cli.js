59083b232646da05d9a655402c08f48d16fc40ea
Philipp Mandler <info@philipp-mandler.com>
2022-03-19 20:40:07 +0100

    cli: Add config_folder argument

--- cli.js
+++ cli.js
@@ -1,4 +1,18 @@
 #!/usr/bin/env node
+const { program } = require('commander');
+const path = require('path');
+
 const { run } = require('./lib/index.js');
 
-run();
+program
+  .argument(
+    '[config_folder]',
+    "the folder with the configuration, containing the 'site' folder",
+    '.',
+  )
+  .action((folder) => {
+    const absPath = path.resolve(folder);
+    run(absPath);
+  });
+
+program.parse();
